80e6a4b6d66577cd74ba779b03949e8bf5485ce8
MichaelRangel <molascuagas@uninorte.edu.co>
2021-09-21 08:26:37 -0500

    Se agrego comentario

--- src/Vertebrado.java
+++ src/Vertebrado.java
@@ -1,30 +1,32 @@
 public class Vertebrado {
-   protected String nombreVertebrado;  
-   protected String claseVertebrado; // valores:mamífero, ave, pez, reptil, anfibio
-   
-   
-   public Vertebrado(String nombreVertebrado, String claseVertebrado){
-       this.nombreVertebrado = nombreVertebrado;
-       this.claseVertebrado = claseVertebrado;
-   }
-   
-      
-   public void setNombreVertebrado( String nombreVertebrado) 
-   {
-       this.nombreVertebrado = nombreVertebrado;
-   }
 
-   public String getNombreVertebrado()
-   {
-       return nombreVertebrado;
-   }
+    //Esto es un cambio
+    protected String nombreVertebrado;  
+     protected String claseVertebrado; // valores:mamífero, ave, pez, reptil, anfibio
 
-   public void setClaseVertebrado( String claseVertebrado) 
-   {
-      this.claseVertebrado = claseVertebrado;
-   }
-   public String getClaseVertebrado()
-   {
-       return claseVertebrado;
-   } 
+
+    public Vertebrado(String nombreVertebrado, String claseVertebrado){
+        this.nombreVertebrado = nombreVertebrado;
+        this.claseVertebrado = claseVertebrado;
+    }
+
+
+    public void setNombreVertebrado( String nombreVertebrado) 
+    {
+        this.nombreVertebrado = nombreVertebrado;
+    }
+
+    public String getNombreVertebrado()
+    {
+        return nombreVertebrado;
+    }
+
+    public void setClaseVertebrado( String claseVertebrado) 
+    {
+        this.claseVertebrado = claseVertebrado;
+    }
+    public String getClaseVertebrado()
+    {
+        return claseVertebrado;
+    } 
 }
